d53c0079b8551db246b631fd5bef1e532d7e487d
Ahmed Youssef <qahmed89@gmail.com>
2024-05-21 02:01:53 +0300

    Add feature 1

--- app/src/main/java/com/example/ci_cdapplication/MainActivity.kt
+++ app/src/main/java/com/example/ci_cdapplication/MainActivity.kt
@@ -4,26 +4,104 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowLeft
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.ci_cdapplication.ui.theme.CICDApplicationTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             CICDApplicationTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "CI - CD World",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                var yourName by remember {
+                    mutableStateOf("")
+                }
+                var name by remember {
+                    mutableStateOf("CI - CD World")
+                }
+                Scaffold(modifier = Modifier.fillMaxSize(),
+                    topBar = {
+                        TopAppBar(
+                            title = { Text(text = "Home") },
+                            colors = TopAppBarDefaults.topAppBarColors(containerColor = Color.Cyan),
+                            navigationIcon = {
+                                IconButton(onClick = { /*TODO*/ }) {
+                                    Icon(
+                                        imageVector = Icons.Filled.KeyboardArrowLeft,
+                                        contentDescription = ""
+                                    )
+                                }
+                            }
+                        )
+                    }
+                ) { innerPadding ->
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Column(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .align(Alignment.CenterStart)
+                                .padding(vertical = 16.dp),
+                            verticalArrangement = Arrangement.Center,
+
+                            ) {
+                            TextField(
+                                label = {Text("Write your Name")},
+                                value = yourName,
+                                onValueChange = { yourName = it },
+                                modifier = Modifier.align(
+                                    Alignment.CenterHorizontally
+                                )
+                            )
+                            Spacer(modifier = Modifier.height(10.dp))
+                            Button(
+                                onClick = { name = yourName }, modifier = Modifier.align(
+                                    Alignment.CenterHorizontally
+                                )
+                            ) {
+                                Text(text = "Say ! Hello")
+                            }
+
+                        }
+                        Greeting(
+                            name = name,
+                            modifier = Modifier.align(alignment = Alignment.BottomCenter)
+                        )
+
+                    }
                 }
             }
         }
